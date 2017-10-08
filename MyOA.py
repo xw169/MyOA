3be63176e27c10a9b998af0d5b5207f46e450e1e
张磊 <lemonz@zhangleideMacBook.local>
2017-10-08 14:22:18 +0800

    Initial commit

---
+++ MyOA.py
@@ -0,0 +1,8 @@
+# ---------------HEAD------------------
+# ProjectName    :    MyOA
+# FileName       :    MyOA
+# CreatedTime    :    2017/10/8 14:21
+# Author         :    lemonz
+# Version        :    V 1.0
+# Describe       :    描述：
+# ---------------CODE------------------
